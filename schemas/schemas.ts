4df3a2b644513464d5b009e2d461fe500c697f3f
Mohit Saud <mohit@prabidhilabs.com>
2024-11-10 20:27:23 +0545

    Add: sop schema

--- schemas/schemas.ts
+++ schemas/schemas.ts
@@ -1,12 +1,5 @@
-// schemas.ts
 import { z } from 'zod';
 
-// export const personalInfoSchema = z.object({
-//   fullName: z.string().optional(),
-//   email: z.string().optional(),
-//   // address: z.string().min(1, 'Address is required'),
-//   phone: z.string().optional(),
-// });
 export const personalInfoSchema = z.object({
   fullName: z.string().min(2, 'Full Name is required'),
   email: z.string().email(),
@@ -26,10 +19,56 @@ export const educationSchema = z.object({
 });
 
 export const skillsSchema = z.object({
-  classroomManagement: z.boolean().optional(),
-  curriculumDevelopment: z.boolean().optional(),
-  lessonPlanning: z.boolean().optional(),
-  assessmentTechniques: z.boolean().optional(),
+  teachingSkills: z
+    .object({
+      classroomManagement: z.boolean().optional(),
+      curriculumDevelopment: z.boolean().optional(),
+      lessonPlanning: z.boolean().optional(),
+      assessmentTechniques: z.boolean().optional(),
+      otherTeachingSkills: z.string().optional(),
+    })
+    .refine(
+      (data) =>
+        data.classroomManagement ||
+        data.curriculumDevelopment ||
+        data.lessonPlanning ||
+        data.assessmentTechniques || // Check if any teaching skill is selected
+        data.otherTeachingSkills, // Check if any teaching skill is selected
+      'Please select or fill in at least one teaching skill'
+    ),
+  culturalKnowledge: z
+    .object({
+      knowledgeOfSpecifiCulturesOrTraditions: z.boolean().optional(),
+      abilityToTeachCulturalValuesAndPerspectives: z.boolean().optional(),
+      fluencyInLanguages: z.boolean().optional(),
+      languageDetails: z.string().optional(),
+      otherCulturalKnowledge: z.string().optional(),
+    })
+    .refine(
+      (data) =>
+        data.knowledgeOfSpecifiCulturesOrTraditions ||
+        data.abilityToTeachCulturalValuesAndPerspectives ||
+        data.fluencyInLanguages ||
+        data.otherCulturalKnowledge, // Check if any cultural knowledge is selected or filled
+      'Please select or fill in at least one cultural knowledge'
+    ),
+  interPersonalSkills: z
+    .object({
+      communicationSkills: z.boolean().optional(),
+      empathy: z.boolean().optional(),
+      patience: z.boolean().optional(),
+      culturalSensitivity: z.boolean().optional(),
+      otherInterpersonalSkills: z.string().optional(),
+    })
+    .refine(
+      (data) =>
+        data.communicationSkills ||
+        data.empathy ||
+        data.patience ||
+        data.culturalSensitivity ||
+        data.otherInterpersonalSkills, // Check if any interpersonal skill is selected or filled
+      'Please select or fill in at least one interpersonal skill'
+    ),
 });
 
 // Convert referencesSchema to an object containing an array
@@ -59,22 +98,29 @@ export const certificationsSchema = z.object({
     .length(2),
 });
 
+export const sopSchema = z.object({
+  statementOfPurpose: z.string().min(50, 'Please provide a brief statement'),
+});
+
 // Combine schemas for final validation
 export const combinedSchema = personalInfoSchema
   .merge(educationSchema)
   .merge(skillsSchema)
+  .merge(sopSchema)
   .merge(completeReferenceSchema)
   .merge(certificationsSchema);
 
 // Types for each form step
 export type PersonalInfo = z.infer<typeof personalInfoSchema>;
 export type Education = z.infer<typeof educationSchema>;
-export type Skills = z.infer<typeof skillsSchema>;
+export type TeachingSkills = z.infer<typeof skillsSchema>;
 export type References = z.infer<typeof completeReferenceSchema>;
 export type Certifications = z.infer<typeof certificationsSchema>;
+export type StatementOfPurpose = z.infer<typeof sopSchema>;
 
 export type CombinedFormData = PersonalInfo &
   Education &
-  Skills &
+  TeachingSkills &
+  StatementOfPurpose &
   References &
   Certifications;
