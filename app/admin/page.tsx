d43496dfcb36d4a2f9153aca6d02423c2f2f46e9
Mohit Saud <mohit@prabidhilabs.com>
2024-11-24 10:10:40 +0545

    Fixed session import

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { checkSession } from '@/utils/checkSession';
+import { checkSession } from '@/utils/CheckSession';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
