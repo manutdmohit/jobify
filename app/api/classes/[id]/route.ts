8575bdffcfe40c35a48e0b5080ea0c55a52fb538
Mohit Saud <mohit@prabidhilabs.com>
2024-11-30 17:20:12 +0545

    Add route for single class

---
+++ app/api/classes/[id]/route.ts
@@ -0,0 +1,37 @@
+import dbConnect from '@/lib/dbConnect';
+import Class from '@/model/Class';
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  await dbConnect();
+
+  console.log(params.id);
+
+  try {
+    const getClass = await Class.findById(params.id);
+
+    console.log(getClass);
+
+    if (!getClass) {
+      return new Response(
+        JSON.stringify({ success: false, message: 'Class not found' }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return new Response(JSON.stringify(getClass), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ success: false, message: 'Internal server error' }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
