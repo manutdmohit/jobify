d65c4841acafa3d935b1d258b020a12c295877b6
Mohit Saud <mohit@prabidhilabs.com>
2024-12-01 08:05:17 +0545

    Add desc to SigninSchema

--- schemas/signInSchema.ts
+++ schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const signInSchema = z.object({
-  identifier: z.string(),
-  password: z.string(),
+  identifier: z.string().email('Please enter a valid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
 });
